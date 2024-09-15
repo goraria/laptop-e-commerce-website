a74bff39841e57c28c1f33b8dad4b71624c5a7c2
LocPhamn <jasonpham20203@gmail.com>
2024-09-15 16:36:40 +0700

    Locst - update example,js and add customedropdown

---
+++ frontend/src/elements/CustomDropDown.js
@@ -0,0 +1,26 @@
+import {
+    Button, Card, Dropdown, DropdownButton, ButtonGroup
+} from "react-bootstrap";
+function CustomDropDown(category) {
+    return (
+        <DropdownButton
+            as={ButtonGroup}
+            key={category.categorical}
+            id={`dropdown-variants-${category.categorical}`}
+            variant={category.varient}
+            title={category.categorical}
+        >
+            {category.item.map(
+                (product) => (
+                    <Dropdown.Item eventKey="1">{product}</Dropdown.Item>
+                )
+            )}
+            
+    
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+        </DropdownButton>
+
+    );
+}
+export default CustomDropDown;
