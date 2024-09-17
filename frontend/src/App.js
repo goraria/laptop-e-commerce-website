d1f8daa8c925f5577bc0cb8b53f555a3cba25c91
Japtor Gortheia <gorariagroup@gmail.com>
2024-09-17 18:46:17 +0700

    Japtor - Update UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Button, Table } from 'react-bootstrap';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 // import logo from './logo.svg';
@@ -10,11 +9,8 @@ import './Example';
 import './pages/Home';
 
 import Header from './components/Header';
-import FormComponent from './components/Form';
 import Footer from './components/Footer';
 import Test from "./Test";
-import Example from "./Example";
-import Home from "./pages/Home";
 
 function App() {
     const [users, setUsers] = useState([]);
@@ -26,13 +22,13 @@ function App() {
             .catch(error => console.error('Error:', error));
     }, []);
     return (
-      <div>
-        <Header />
-        <div className="container">
-            <Test />
+        <div>
+            <Header />
+            <div className="container">
+                <Test />
+            </div>
+            <Footer />
         </div>
-        <Footer />
-      </div>
     );
 }
 
