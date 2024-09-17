cc7e7577db13b29ed970f4ab853ae3b267865448
LocPhamn <jasonpham20203@gmail.com>
2024-09-17 18:08:32 +0700

    Locst - add ProductIten.js

--- frontend/src/elements/ProductItem.js
+++ frontend/src/elements/ProductItem.js
@@ -1,20 +1,38 @@
+import { Component } from "react";
 import {
     Button, Card, Dropdown, DropdownButton, ButtonGroup
 } from "react-bootstrap";
 
-function BasicExample() {
-    return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-    );
+const obj_test = {
+    name: "Product 1",
+    price: "5$",
+    decription: "scipt",
+    image: "src"
 }
-export default BasicExample;
+
+class ProductItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+    render() {
+        const { obj } = this.props; 
+        return (
+            <Card >
+                <Card.Img variant="top" src={obj.image} />
+                <Card.Body>
+                    <Card.Title>{obj.name}</Card.Title>
+                    <Card.Text> {obj.description} </Card.Text>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <span style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>${obj.price}</span>
+                        <Button variant="danger" style={{ float: 'right' }}>Buy now</Button>
+                    </div>
+                </Card.Body>
+            </Card>
+        );
+    }
+
+}
+export default ProductItem;
