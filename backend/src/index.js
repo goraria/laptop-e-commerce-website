966139142b1fd8ea1381dcd4057ec8cd9337c1a1
pa106 <zzphananhzz@gmail.com>
2024-09-19 16:37:23 +0700

    Payhd - set frontend port, send data from front-end to back-end and add express-session

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { Session } = require('inspector');
 // const apiRoutes = require('./routes/api');
-
+const session = require('express-session');
 const app = express();
-const port = 9900;
-
+const port = 9999;
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 // app.use('/api', apiRoutes);
 
@@ -19,14 +20,14 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.post('/submit', (req, res) => {
     const { name, email } = req.body;
+
     console.log('Received data:', { name, email });
     res.json({ message: 'Data received successfully', data: { name, email } });
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
-
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+// });
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
