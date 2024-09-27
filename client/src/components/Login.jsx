925ca8a47be4fac71b9262e14997b57c5e376294
goraria <gorariagroup@gmail.com>
2024-09-27 16:34:36 +0700

    Japtor - Configuration makeup file and change js file to jsx file

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
-import {Button, Carousel, Col, Container, Dropdown, Form, Image, Row} from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import {Button, Col, Container, Form, Image, Row} from "react-bootstrap";
 import {
     faApple, faMeta, faGoogle, faTwitter, faXTwitter, faGithub
 } from '@fortawesome/free-brands-svg-icons';
-import SocialFormButton from "../elements/SocialFormButton";
 
-import jp from "../resources/images/jp.jpeg";
-import {Link} from "react-router-dom";
+import SocialFormButton from "../elements/SocialFormButton.jsx";
+
+import jp from '../resources/images/jp.jpeg'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const sclItems = [
     // { id: 0, name: "Github", icon: faGithub, color: "secondary" },
@@ -16,7 +18,7 @@ const sclItems = [
     // { id: 4, name: "Twitter", icon: faTwitter },
 ]
 
-class Signup extends Component {
+class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -36,7 +38,7 @@ class Signup extends Component {
                             backgroundColor: '#f8f9fa', // backgroundColor: '#eaedf0'
                         }}>
                             <div>
-                                <h3>Sign up</h3>
+                                <h3>Log in</h3>
                                 <div style={{display: "flex", marginBottom: 16, justifyContent: 'center'}}>
                                     <Image
                                         className="d-block"
@@ -70,29 +72,45 @@ class Signup extends Component {
                                             </Button>
                                         </Col>
                                     </Row>
-                                    <Link to="/register">
+                                    <Link to="/">
                                         <Button variant="danger" type="submit" className="mb-3" style={{width: '100%'}}>
-                                            Sign up
+                                            Log in
                                         </Button>
                                     </Link>
                                     <hr/>
-                                    <div className="text-center" style={{marginBottom: 16}}>or sign up with</div>
+                                    <div className="text-center" style={{marginBottom: 16}}>or sign in with</div>
                                     <div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
+                                        {/*<Button variant="primary" type="button"*/}
+                                        {/*        style={{flex: '1', margin: '0 5px 10px 0', minWidth: '120px'}}>*/}
+                                        {/*    <i className="fab fa-facebook" style={{marginRight: 8}}></i> Facebook*/}
+                                        {/*</Button>*/}
+                                        {/*<Button variant="outline-dark" type="button"*/}
+                                        {/*        style={{flex: '1', margin: '0 5px 10px 0', minWidth: '120px'}}>*/}
+                                        {/*    <i className="fab fa-google" style={{marginRight: 8}}></i> Google*/}
+                                        {/*</Button>*/}
+                                        {/*<Button variant="info" type="button"*/}
+                                        {/*        style={{flex: '1', margin: '0 5px 10px 0', minWidth: '120px'}}>*/}
+                                        {/*    <i className="fab fa-twitter" style={{marginRight: 8}}></i> Twitter*/}
+                                        {/*</Button>*/}
+                                        {/*<Button variant="secondary" type="button"*/}
+                                        {/*        style={{flex: '1', margin: '0 5px 10px 0', minWidth: '120px'}}>*/}
+                                        {/*    <i className="fab fa-linkedin" style={{marginRight: 8}}></i> LinkedIn*/}
+                                        {/*</Button>*/}
                                         {sclItems.map(socialItem => (
                                             <SocialFormButton key={socialItem.id} socialItems={socialItem}/>
                                         ))}
                                     </div>
                                     <hr/>
                                     <div className="text-center" style={{marginBottom: 16}}>
-                                        you have an acoount
-                                        <Link to="/login">
+                                        you don't have an acoount
+                                        <Link to="/signup">
                                             <Button variant="link" style={{
                                                 padding: 0,
                                                 color: '#dc3545',
                                                 textDecoration: "none",
                                                 fontWeight: 'bold',
                                                 paddingLeft: 4
-                                            }}>Log in</Button>
+                                            }}>Sign up</Button>
                                         </Link>
                                     </div>
                                 </Form>
@@ -105,4 +123,9 @@ class Signup extends Component {
     }
 }
 
-export default Signup
+export default Login
+
+// a. Bạn có muốn thêm tooltip khi hover vào các nút mạng xã hội không?
+//     b. Bạn có muốn sử dụng hiệu ứng khi bấm nút để tăng tính thẩm mỹ không?
+//     c. Bạn có muốn thêm hiệu ứng hover để làm nổi bật các nút mạng xã hội khi di chuột qua không?
+//     d. Bạn có muốn thay đổi màu sắc cho từng nút mạng xã hội (để đồng bộ với thương hiệu từng mạng như màu xanh của Facebook, màu đỏ của Google, v.v.) không?
