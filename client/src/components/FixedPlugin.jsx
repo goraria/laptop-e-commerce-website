1a41da5379fa18d2fb497be5cd0529a7acbfa75b
pa106 <zzphananhzz@gmail.com>
2024-09-28 23:47:05 +0700

    Payhd - Create user manage, product manage front-end

--- client/src/components/FixedPlugin.jsx
+++ client/src/components/FixedPlugin.jsx
@@ -1,26 +1,10 @@
-/*!
 
-=========================================================
-* Black Dashboard React v1.2.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 
 // reactstrap components
 import { Button, Dropdown, DropdownToggle, Badge } from "reactstrap";
-import { ThemeContext, themes } from "contexts/ThemeContext";
-import { backgroundColors } from "contexts/BackgroundColorContext";
+import { ThemeContext, themes } from "./ThemeContext";
+import { backgroundColors } from "./BackgroundColorContext";
 
 function FixedPlugin(props) {
   const [dropDownIsOpen, setdropDownIsOpen] = React.useState(false);
