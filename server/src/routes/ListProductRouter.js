bce63889ee2be403b3d3905374cc6d9480dd818c
LocPhamn <jasonpham20203@gmail.com>
2024-09-30 21:11:54 +0700

    Locst - config product router, controller

--- server/src/routes/ListProductRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const listProductController = require('../controllers/ListProductController');
-
-// Route để lấy tất cả người dùng
-router.get('/getAllProduct', listProductController.getAllProduct);
-
-module.exports = router;
