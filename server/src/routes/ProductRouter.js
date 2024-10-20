b1f465f8484fcf93a13b16a42e25d351de6aeea4
LocPhamn <jasonpham20203@gmail.com>
2024-10-20 16:51:20 +0700

    Locst - update product

--- server/src/routes/ProductRouter.js
+++ server/src/routes/ProductRouter.js
@@ -4,4 +4,11 @@ const productController = require('../controllers/ProductController');
 
 // Route để lấy tất cả người dùng
 router.get('/load-product', productController.loadProduct);
+router.get('/load-product/:idProduct', productController.loadProductWithID);
+router.get('/load-description/:idProduct', productController.loadDescription);
+router.get('/load-configuration/:idProduct', productController.loadConfiguration);
+router.get('/load-rating/:idProduct', productController.loadRating);
+
+
+
 module.exports = router;
